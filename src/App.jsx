45beb02f22b3d621e6694ff6a3d349120843be50
Berna Kurt <beekurt98@gmail.com>
2025-03-25 17:46:30 +0300

    todo eklendi

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,15 @@
 import { useState } from 'react'
 import './App.css'
 
+// Todo:
+// login-sign up-sign out
+// sign up: isim-soyisim, tel no (auth ve users tablosu)
+// kategorilerine ürünleri sıralama
+// carta ekleme
+// checkout dedikten sonra adres kısmı kontrolü
+// orders ve order_details tablolarına ürün gönderme
+// ekstralar: notifications
+
 function App() {
 
   return (
