906957b22efbd1d2e8fe2c316095406ba3f13afc
Berna Kurt <beekurt98@gmail.com>
2025-03-26 17:00:09 +0300

    supabase orders ve order_details tablolarına ekleme yapıldı

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,8 +1,11 @@
 import { useContext, useEffect, useState } from "react"
 import { SupabaseContext } from "../App"
+import { useNavigate } from "react-router-dom";
 
 export default function Cart() {
   const { supabase, cart, setCart, cartObj, setCartObj } = useContext(SupabaseContext);
+  const [totalPrice, setTotalPrice] = useState(0);
+  const navigate = useNavigate();
 
   useEffect(() => {
     if (!Array.isArray(cart)) return; 
@@ -28,7 +31,36 @@ export default function Cart() {
     localStorage.setItem("cartObj", JSON.stringify(newCartObj));
   }, [cart]);
   
-  
+  async function completeOrder() {
+    const { data, error } = await supabase
+    .from('orders')
+    .insert([
+      { paid_price: calculatePrice(), status_id: 1 },
+    ])
+    .select()
+
+    console.log(data);
+
+    const orderDetails = cart.map(item =>{ return {
+      order_id: data[0].id, 
+      product_id: item.id
+    }})
+    
+    const { data:order_details, err } = await supabase
+    .from('order_details')
+    .insert(orderDetails)
+    .select()
+
+    localStorage.clear();
+    navigate("/");
+  }
+
+  function calculatePrice() {
+    let fullPrice = 0;
+    cart.map(x => fullPrice = fullPrice + x.price);
+    setTotalPrice(fullPrice);
+    return fullPrice;
+  }
   
   return (
     <>
@@ -41,7 +73,7 @@ export default function Cart() {
           <p>miktar: {cartObj[x]?.quantity}</p>
         </div>)
       }
-      <button>Sipariş Ver</button>
+      <button onClick={completeOrder}>Sipariş Ver</button>
     </>
   )
 }
