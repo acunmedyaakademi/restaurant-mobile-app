d0d74686a191f44feb2830fe20bc418b64f18af3
Berna Kurt <beekurt98@gmail.com>
2025-03-29 18:54:19 +0300

    anasayfaya preview ürünler eklendi

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,26 +1,57 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom"
 import { SupabaseContext } from "../App";
 import '../css/Home.css'
 
 export default function Home() {
-  const { authUser } = useContext(SupabaseContext);
+  const { authUser, supabase } = useContext(SupabaseContext);
+  const [previewProducts, setPreviewProducts] = useState([]);
   const navigate = useNavigate();
+
+  useEffect(() => {
+    async function getProducts() {
+      let { data, error } = await supabase
+        .from('products')
+        .select('*')
+        .range(0, 5)
+      setPreviewProducts(data);
+    }
+
+    getProducts();
+  }, [])
 
   return (
     <>
       <div className="home-page">
         <img src="" alt="" />
-        <h2>Welcome to our restaurant</h2>
+        <h2>Hızlı, Sıcak, Lezzetli!</h2>
         <div className="user-controls">
           {
             authUser
-              ? <p>{authUser?.name}</p>
+              ? <p>Merhaba, {authUser?.name}</p>
               : <>
                 <button onClick={() => navigate("/login")}>Login</button>
                 <button onClick={() => navigate("/signup")}>Signup</button>
               </>
           }
+
+        </div>
+        <div className="preview-products">
+          <div className="preview-header">
+            <h3>Ürünler</h3>
+            <a href="/products">Göz atın</a>
+          </div>
+          <div className="products-list">
+            {
+              previewProducts.map(x => <div className="product-list-item">
+                <div className="product-item-img">
+                  <img src={x?.img} />
+                </div>
+                <h4>{x?.name}</h4>
+                <span>₺{x?.price}</span>
+              </div>)
+            }
+          </div>
 
         </div>
       </div>
