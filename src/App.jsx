d4acaff1d83fb0a126798706d9f13429101286cd
Berna Kurt <beekurt98@gmail.com>
2025-04-02 18:24:07 +0300

    adres güncellemenin bir kısmı eklendi

--- src/App.jsx
+++ src/App.jsx
@@ -25,6 +25,7 @@ import PastOrders from './profile-pages/PastOrders'
 // profiles policy değiştir (enable users to view their data only)
 // eğer supabase kioska hata veriyorsa user_id kısmını gözden geçir
 // loading ekle
+// edit adres'te seçilen adresin bilgileri input alanına gelecek
 
 export const supabase = createClient('https://sxkbwpcardxrhfuqzvzc.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InN4a2J3cGNhcmR4cmhmdXF6dnpjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDI0NzQ3MjAsImV4cCI6MjA1ODA1MDcyMH0.f6pWVT3SGve_Xmcs_m2lH0YDX9anp3hI915eNgjfgTI')
 
--- src/profile-pages/Addresses.jsx
+++ src/profile-pages/Addresses.jsx
@@ -1,4 +1,4 @@
-import { useContext, useEffect } from "react";
+import { useContext, useEffect, useRef } from "react";
 import { useState } from "react"
 import { SupabaseContext } from "../App";
 
@@ -6,18 +6,31 @@ export default function Addresses() {
   const { supabase, userId } = useContext(SupabaseContext);
   const [addresses, setAddresses] = useState([]);
   const [addNew, setAddNew] = useState(false);
+  const editDialogRef = useRef(null);
+  const deleteDialog = useRef(null);
+  const [selectedAddressId, setSelectedAddressId] = useState(null);
 
   useEffect(() => {
     async function getAddresses() {
       let { data, error } = await supabase
-      .from('addresses')
-      .select('*')
+        .from('addresses')
+        .select('*')
       setAddresses(data);
-              
+
     }
 
     getAddresses();
   }, [])
+
+  async function deleteAddress(e) {
+    e.preventDefault();
+    const { error } = await supabase
+      .from('addresses')
+      .delete()
+      .eq('id', selectedAddressId)
+    deleteDialog.current.close();
+
+  }
 
 
   return (
@@ -34,19 +47,26 @@ export default function Addresses() {
                   addresses.length == 0
                     ? <div>Kayıtlı Adres Bulunamadı.</div>
                     : <>
-                    {
-                      addresses.map(x => <div>
-                        <h3>{x.adres_basligi}</h3>
-                        <p>{x.mahalle}, {x.ilce}, {x.il}, {x.adres}</p>
-                        <button>Sil</button>
-                        <button>Düzenle</button>
-                      </div>)
-                    }
+                      {
+                        addresses.map(x => <div>
+                          <h3>{x.adres_basligi}</h3>
+                          <p>{x.mahalle}, {x.ilce}, {x.il}, {x.adres}</p>
+                          <button onClick={() => { deleteDialog.current.showModal(); setSelectedAddressId(x.id) }}>Sil</button>
+                          <button onClick={() => editDialogRef.current.showModal()}>Düzenle</button>
+                        </div>)
+                      }
                     </>
                 }
               </div>
             </>
         }
+        <dialog ref={deleteDialog}>
+          <h3>Seçili Adres Silinecek. Emin Misiniz?</h3>
+          <button onClick={(e) => deleteAddress(e)}>Adresi Sil</button>
+          <button onClick={() => deleteDialog.current.close()}>Vazgeç</button>
+
+        </dialog>
+        <EditDialog editDialogRef={editDialogRef} selectedAddressId={selectedAddressId} setSelectedAddressId={setSelectedAddressId} />
       </div>
     </>
   )
@@ -62,13 +82,13 @@ function NewAddressForm({ setAddNew }) {
     const { adres_basligi, il, ilce, mahalle, adres } = formObj;
 
     const { data, error } = await supabase
-    .from('addresses')
-    .insert([
-      { user_id: userId, adres_basligi, il, ilce, mahalle, adres },
-    ])
-    .select()
+      .from('addresses')
+      .insert([
+        { user_id: userId, adres_basligi, il, ilce, mahalle, adres },
+      ])
+      .select()
     setAddNew(false);
-            
+
   }
 
   return (
@@ -83,5 +103,56 @@ function NewAddressForm({ setAddNew }) {
         <button>Adres Ekle</button>
       </form>
     </>
+  )
+}
+
+function EditDialog({ editDialogRef, selectedAddressId, setSelectedAddressId }) {
+  const { supabase, userId } = useContext(SupabaseContext);
+  const [address, setAddress] = useState({});
+
+  useEffect(() => {
+    async function getCurrentAddress() {
+
+      let { data, error } = await supabase
+        .from('addresses')
+        .select('*')
+        .eq('id', selectedAddressId)
+      setAddress(data)
+      console.log(data);
+      
+
+    }
+
+    getCurrentAddress();
+
+  }, [])
+
+
+  async function handleEditAddress(e) {
+    e.preventDefault();
+    const formData = new FormData(e.target);
+    const formObj = Object.fromEntries(formData);
+    const { adres_basligi, il, ilce, mahalle, adres } = formObj;
+    const { data, error } = await supabase
+      .from('addresses')
+      .update({ adres_basligi, il, ilce, mahalle, adres })
+      .eq('user_id', userId)
+      .select()
+
+    editDialogRef.current.close();
+  }
+
+  return (
+    <dialog ref={editDialogRef}>
+      Güncelle
+      <form onSubmit={handleEditAddress}>
+        <input type="text" name="adres_basligi" placeholder="adres başlığı" defaultValue={address?.adres_basligi} />
+        <input type="text" name="il" placeholder="il" defaultValue={address?.il} />
+        <input type="text" name="ilce" placeholder="ilçe" defaultValue={address?.ilce} />
+        <input type="text" name="mahalle" placeholder="mahalle" defaultValue={address?.mahalle} />
+        <input type="text" name="adres" placeholder="adres" defaultValue={address?.adres} />
+        <button>Adresi Güncelle</button>
+      </form>
+    </dialog>
   )
 }
